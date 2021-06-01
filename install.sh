7ee684f74491cc986ba24e03610cc6cdca37207a
EC2 Default User <ec2-user@ip-172-31-29-8.ap-southeast-1.compute.internal>
2021-06-01 13:23:21 +0000

    Complete install.sh to be compatible with scrapper unit

--- install.sh
+++ install.sh
@@ -4,6 +4,44 @@
 # Script to install mic files
 # Only required for those who do not know how to deploy
 #==============================
+
+#==============================
+# Helper Functions
+#==============================
+
+#==============================
+# logTitle
+#=============================
+
+logTitle()
+{
+	local x=${1}
+	printf "=========================================\n"
+        printf "==== $x ====\n"
+	printf "=========================================\n"
+}
+
+#==============================
+# logOut
+#=============================
+
+logOut()
+{
+	local x=${1}
+	printf "INFO: $x\n"
+}
+
+#==============================
+# logWarn
+#==============================
+
+logWarn()
+{
+	local x=${1}
+	printf "WARNING: $x\n"
+}
+
+#===============================
 source config/env.config
 printf "=========================================\n"
 printf "=== Installation of dashboard finance ===\n"
@@ -14,6 +52,20 @@ if [ ! -f $SECRET_KEYS ]; then
     mkdir -p $SECRET_KEYS_DIR
     touch $SECRET_KEYS
 fi
+
+#TODO 
+#HDB FOLDER CREATION
+logOut "Checking if HDB Folder is present..."
+if [ ! -f $HDB_DIR ] ; then
+	logWarn "$HDB_DIR is missing, creating folder"
+	mkdir -p $HDB_DIR
+fi
+
+logOut "Checking if HDB Daily Folder is present..."
+if [ ! -f $HDB_DAILY_DIR ] ; then
+	logWarn "$HDB_DAILY_DIR is missing, creating folder"
+	mkdir -p $HDB_DAILY_DIR
+fi 
 
 printf "INFO:Current release support installation of 2 API KEYS\n Please select what are you trying to install\n 1)Alphavantage \n 2)Finnhub\n"
 read choice
